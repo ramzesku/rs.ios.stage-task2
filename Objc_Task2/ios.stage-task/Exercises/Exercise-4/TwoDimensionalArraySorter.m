d24ab388b26113941e5071aeb11559b97645e46c
Siarhei Lysiuk <siarhei.lysiuk@p97.com>
2021-05-17 20:46:35 +0300

    feat: complete exercise-4

--- Objc_Task2/ios.stage-task/Exercises/Exercise-4/TwoDimensionalArraySorter.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-4/TwoDimensionalArraySorter.m
@@ -2,8 +2,57 @@
 
 @implementation TwoDimensionalArraySorter
 
+typedef NS_ENUM(NSUInteger, ArrayType) {
+    mixed,
+    string,
+    number,
+    unknown
+};
+
 - (NSArray *)twoDimensionalSort:(NSArray<NSArray *> *)array {
-    return @[];
+    // Get Array type
+    ArrayType arrayType = [self getArrayType:array];
+    if (arrayType != string && arrayType != number) {
+        return @[];
+    }
+    
+    // Create one array
+    NSMutableArray *result = [NSMutableArray new];
+    for (NSArray *arrayItems in array) {
+        for (NSObject *item in arrayItems) {
+            [result addObject:item];
+        }
+    }
+    
+    // Sort Array array
+    NSSortDescriptor *sortDescriptor = [[NSSortDescriptor alloc] initWithKey:nil ascending:YES];
+    NSArray *sortDescriptors = [NSArray arrayWithObject:sortDescriptor];
+    NSArray *sortedArray = [result sortedArrayUsingDescriptors:sortDescriptors];
+    return sortedArray;
+}
+
+- (ArrayType)getArrayType:(NSArray<NSArray *> *)array {
+    NSString *type = nil;
+    
+    for (int i=0; i<array.count; i++) {
+        NSString *itemsType = NSStringFromClass([array[i] class]);
+        
+        // Check array[i] is Array
+        if (![itemsType isEqualToString:@"__NSArrayI"]) { return unknown; }
+        
+        for (NSObject *items in array[i]) {
+            NSString *itemsType = NSStringFromClass([items class]);
+            if (type == nil) { type = itemsType; }
+            if (![itemsType isEqualToString:type]) { return mixed; }
+        }
+    }
+    
+    if ([type isEqualToString:@"__NSCFConstantString"]) {
+        return string;
+    } else if ([type isEqualToString:@"__NSCFNumber"]) {
+        return number;
+    }
+    return unknown;
 }
 
 @end
