e836b0f451d73138f8397f67a9977ded3b738880
Siarhei Lysiuk <siarhei.lysiuk@p97.com>
2021-05-17 20:46:22 +0300

    feat: complete exercise-3

--- Objc_Task2/ios.stage-task/Exercises/Exercise-3/IPFormatConverter.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-3/IPFormatConverter.m
@@ -3,7 +3,27 @@
 @implementation IPFormatConverter
 
 - (NSString *)ipFormatConverter:(NSArray *)numbersArray {
-    return nil;
+    
+    if (numbersArray.count == 0) {
+        return @"";
+    }
+    
+    NSMutableArray<NSNumber *> *ip = @[@0, @0, @0, @0].mutableCopy;
+    
+    for (int i = 0; i < numbersArray.count && i < 4; i++) {
+        NSNumber *number = numbersArray[i];
+        if (number == nil) {
+            return @"Is not number";
+        } else if (number.integerValue < 0) {
+            return @"Negative numbers are not valid for input.";
+        } else if (number.integerValue > 255) {
+            return @"The numbers in the input array can be in the range from 0 to 255.";
+        }
+        
+        ip[i] = number;
+    }
+    
+    return [NSString stringWithFormat:@"%@.%@.%@.%@", ip[0], ip[1], ip[2], ip[3]];
 }
 
 @end
