20f0fab02235e8edd9e780d77623e3357239c37e
Siarhei Lysiuk <siarhei.lysiuk@p97.com>
2021-05-18 00:03:34 +0300

    fix: exercise-4

--- Objc_Task2/ios.stage-task/Exercises/Exercise-4/TwoDimensionalArraySorter.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-4/TwoDimensionalArraySorter.m
@@ -2,57 +2,44 @@
 
 @implementation TwoDimensionalArraySorter
 
-typedef NS_ENUM(NSUInteger, ArrayType) {
-    mixed,
-    string,
-    number,
-    unknown
-};
-
 - (NSArray *)twoDimensionalSort:(NSArray<NSArray *> *)array {
-    // Get Array type
-    ArrayType arrayType = [self getArrayType:array];
-    if (arrayType != string && arrayType != number) {
-        return @[];
-    }
-    
-    // Create one array
-    NSMutableArray *result = [NSMutableArray new];
-    for (NSArray *arrayItems in array) {
-        for (NSObject *item in arrayItems) {
-            [result addObject:item];
-        }
-    }
-    
-    // Sort Array array
-    NSSortDescriptor *sortDescriptor = [[NSSortDescriptor alloc] initWithKey:nil ascending:YES];
-    NSArray *sortDescriptors = [NSArray arrayWithObject:sortDescriptor];
-    NSArray *sortedArray = [result sortedArrayUsingDescriptors:sortDescriptors];
-    return sortedArray;
-}
 
-- (ArrayType)getArrayType:(NSArray<NSArray *> *)array {
-    NSString *type = nil;
+    NSMutableArray *numbers = [NSMutableArray new];
+    NSMutableArray *strings = [NSMutableArray new];
     
     for (int i=0; i<array.count; i++) {
-        NSString *itemsType = NSStringFromClass([array[i] class]);
-        
-        // Check array[i] is Array
-        if (![itemsType isEqualToString:@"__NSArrayI"]) { return unknown; }
+        if (![array[i] isKindOfClass:[NSArray class]]) {
+            return @[];
+        }
         
         for (NSObject *items in array[i]) {
-            NSString *itemsType = NSStringFromClass([items class]);
-            if (type == nil) { type = itemsType; }
-            if (![itemsType isEqualToString:type]) { return mixed; }
+            if ([items isKindOfClass:[NSString class]]) {
+                [strings addObject:items];
+            } else if ([items isKindOfClass:[NSNumber class]]) {
+                [numbers addObject:items];
+            }
         }
     }
     
-    if ([type isEqualToString:@"__NSCFConstantString"]) {
-        return string;
-    } else if ([type isEqualToString:@"__NSCFNumber"]) {
-        return number;
+    NSSortDescriptor *sortAscending = [[NSSortDescriptor alloc] initWithKey:nil ascending:YES];
+    NSArray *arrayAscending = [NSArray arrayWithObject:sortAscending];
+    NSSortDescriptor *sortDescending = [[NSSortDescriptor alloc] initWithKey:nil ascending:NO];
+    NSArray *arrayDescending = [NSArray arrayWithObject:sortDescending];
+    
+    NSMutableArray *result = [NSMutableArray new];
+    if (numbers.count > 0 && strings.count > 0) {
+        NSArray *sortedNumbers = [numbers sortedArrayUsingDescriptors:arrayAscending];
+        NSArray *sortedStrings = [strings sortedArrayUsingDescriptors:arrayDescending];
+        result = @[sortedNumbers, sortedStrings].mutableCopy;
+    } else if (numbers.count > 0) {
+        NSArray *sortedNumbers = [numbers sortedArrayUsingDescriptors:arrayAscending];
+        result = sortedNumbers.mutableCopy;
+    } else if (strings.count > 0) {
+        NSArray *sortedStrings = [strings sortedArrayUsingDescriptors:arrayAscending];
+        result = sortedStrings.mutableCopy;
     }
-    return unknown;
+    
+    return result;
 }
 
 @end
