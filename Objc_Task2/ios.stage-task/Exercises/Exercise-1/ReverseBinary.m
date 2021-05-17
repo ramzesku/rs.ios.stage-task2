6d9860c94ec37fb6e70d78a258b9b4ffdc2f627d
Siarhei Lysiuk <siarhei.lysiuk@p97.com>
2021-05-17 20:44:07 +0300

    init: start task and complete exercise-1

--- Objc_Task2/ios.stage-task/Exercises/Exercise-1/ReverseBinary.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-1/ReverseBinary.m
@@ -1,5 +1,18 @@
 #import "ReverseBinary.h"
 
 UInt8 ReverseInteger(UInt8 n) {
-    return 0;
+    NSString *string = @"" ;
+    UInt8 x = n;
+
+    while (x>0) {
+        string = [string stringByAppendingString:[NSString stringWithFormat: @"%d", x&1]];
+        x = x >> 1;
+    }
+    
+    while ([string length] < 8) {
+        string = [string stringByAppendingString: @"0"];
+    }
+    
+    UInt8 result = strtol([string UTF8String], NULL, 2);
+    return result;
 }
