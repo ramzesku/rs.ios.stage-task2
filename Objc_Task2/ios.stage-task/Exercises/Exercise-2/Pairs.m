b1fbd18b0e17aa06059b678edf891a1a3f3361c7
Siarhei Lysiuk <siarhei.lysiuk@p97.com>
2021-05-17 20:45:52 +0300

    feat: complete exercise-2

--- Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
@@ -3,7 +3,16 @@
 @implementation Pairs
 
 - (NSInteger)countPairs:(NSArray <NSNumber *> *)array number:(NSNumber *)number {
-    return 0;
+    NSInteger difference = number.integerValue;
+    NSInteger count = 0;
+    for (int i = 0; i < array.count - 1; i++) {
+        for (int j = i + 1; j < array.count; j++) {
+            if (labs(array[j].integerValue - array[i].integerValue) == difference) {
+                count++;
+            }
+        }
+    }
+    return count;
 }
 
 @end
